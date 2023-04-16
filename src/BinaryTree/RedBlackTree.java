eaa4f3d44cb18c260437f87d77081cf00568b451
GIL <gil.00000032610@unicap.br>
2023-04-16 17:44:30 -0300

    Method to update references on newChild and parent

--- src/BinaryTree/RedBlackTree.java
+++ src/BinaryTree/RedBlackTree.java
@@ -18,7 +18,14 @@ public class RedBlackTree extends ArvoreBinariaDeBusca {
         if (parent.isBlack()) {
             return;
         }
-        Node uncle = getUncle(node);
+        Node uncle = getUncle(parent);
+        Node grandparent = parent.getParent();
+        if (uncle != null && !uncle.isBlack()) {
+            parent.setColor(Node.BLACK_COLOR);
+            uncle.setColor(Node.BLACK_COLOR);
+            grandparent.setColor(Node.RED_COLOR);
+            fixPostInsertion(grandparent);
+        }
 
     }
 
@@ -30,4 +37,40 @@ public class RedBlackTree extends ArvoreBinariaDeBusca {
         return grandparent.getLeft();
     }
 
+    private void rotateLeft(Node node) {
+        Node parent = node.getParent();
+        Node rightChild = node.getRight();
+
+        node.setRight(rightChild.getLeft());
+
+        if (rightChild.getLeft() != null) {
+            rightChild.getLeft().setParent(node);
+        }
+        rightChild.setLeft(node);
+        node.setParent(rightChild);
+        changeReferences(parent, node, rightChild);
+    }
+
+    private void rotateRight(Node grandparent) {
+        Node y = grandparent.getRight();
+        grandparent.setRight(y.getLeft());
+        y.setLeft(grandparent);
+    }
+
+    private void changeReferences(Node parent, Node oldChild, Node newChild) {
+        if (parent == null) {
+            this.raiz = newChild;
+        } else if (oldChild == parent.getRight()) {// Verifica se o antigo filho é referenciado na subárvore direita
+            parent.setRight(newChild);// Muda a referência da subárvore direita do pai para o novo nó
+
+        } else if (oldChild == parent.getLeft()) {// Verifica se o antigo filho é referenciado na subárvore esquerda
+            parent.setLeft(newChild);// Muda a referência da subárvore esquerda do pai para o novo nó
+
+        }
+        if (newChild != null) {// Caso o novo nó Não seja nulo, a referência para o pai do nó agora é o novo
+                               // pai
+            newChild.setParent(parent);
+        }
+
+    }
 }
